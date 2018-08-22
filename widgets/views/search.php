fcc7cc0a0d298f325aed9b623aaf6edecc2aae58
miha-coder <miclee123@mail.ru>
2018-08-22 19:58:41 +0300

    translate keyword

--- widgets/views/search.php
+++ widgets/views/search.php
@@ -1,9 +1,9 @@
-<form action="<?php echo Yii::$app->getUrlManager()->createUrl(["/blog/default/index"]) ?>" method="get">
+<form action="<?= Yii::$app->getUrlManager()->createUrl(["/blog/default/index"]) ?>" method="get">
     <div class="search">
         <input type="text" name="keyword" id="keyword"
                value="<?php if (isset($_GET['keyword']) && strlen($_GET['keyword']) > 0) echo $_GET['keyword']; else echo 'Слово для поиска'; ?>"
                class="search_input" onblur="if(this.value=='') this.value='Слово для поиска';"
-               onfocus="if(this.value=='内容关键词') this.value='';"/>
+               onfocus="if(this.value=='Слово для поиска') this.value='';"/>
         <input type="submit" class="search_submit" id="search_submit" value="搜索"/>
     </div>
 </form>
